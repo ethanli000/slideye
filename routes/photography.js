471d335bb5bf38362ba9b2406686d96bb37992eb
liincn <taozi8955@gmail.com>
2014-10-25 23:16:03 +0900

    add

---
+++ routes/photography.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  var photoProjectList = new Array();
+//var a;
+
+// a  = (function factorial(n) {
+//     return (n <= 1)
+//     ? 1
+//     : factorial(n - 1) * n;
+// })(10);
+
+
+  photoProjectList[0] = (function(id,name){
+    var photoProject = new Array();
+    photoProject["pid"] = id;
+    photoProject["title"] = name;
+    return photoProject;
+  })(1,"exotic tokyo");
+
+  photoProjectList[1] = (function(id,name){
+    var photoProject = new Array();
+    photoProject["pid"] = id;
+    photoProject["title"] = name;
+    return photoProject;
+  })(2,"hello, kamakura");
+  res.render('photography', { photoProjectList: photoProjectList });
+});
+
+module.exports = router;
